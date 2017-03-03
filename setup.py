c76a28f869d8605e531d7dc706c8965036bd2c60
Eric Crawford <eric.crawford@mail.mcgill.ca>
2017-03-03 17:28:56 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clify',
+      version='0.1',
+      description='',
+      url='http://github.com/e2crawfo/clify',
+      author='Eric Crawford',
+      author_email='eric.crawford@mail.mcgill.ca',
+      license='MIT',
+      packages=['clify'],
+      zip_safe=False)
